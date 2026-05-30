1e0deb4b2ddf6961e746d6bce193fd42421cc8ba
mariana florentino <marimar400v@gmail.com>
2026-05-30 16:02:18 -0300

    Update server.js

--- server.js
+++ server.js
@@ -101,7 +101,8 @@ app.post('/api/login', (req, res) => {
 });
 
 // Inicialização do Servidor Local
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Servidor do Study.ia rodando com sucesso na porta ${PORT}`);
+const port = process.env.PORT || 3000;
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Servidor rodando na porta ${port}`);
 });
